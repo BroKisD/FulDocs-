0cda9c0498f8ae07253281cf9bae5f8d6af3fa16
BroKisD <khoaphan2601@gmail.com>
2025-12-02 00:37:32 +0700

    add port binding

--- app.py
+++ app.py
@@ -794,4 +794,5 @@ def uploaded_file(filename: str):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 9000))
+    app.run(host='0.0.0.0', port=port, debug=os.environ.get('FLASK_ENV') == 'development')
